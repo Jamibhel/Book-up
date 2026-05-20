0b6d3b5072af4badacad602da76d893e8918acde
Jamibhel <Jamibelbhello0104@gmail.com>
2026-05-21 00:19:55 +0100

    design: complete overhaul to Deep Green Dark Mode across all platforms and unified theme enforcement

--- web/dev-dist/sw.js
+++ web/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t8m47t2jo7o"
+    "revision": "0.h9l2s20afgg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
